7aab6c4150cdefce6a35275c8235b05769b3cb58
Louie Constable <Lzc5535@psu.edu>
2023-08-21 16:27:15 -0400

    yahoo changes

--- TickerPlant/store.py
+++ TickerPlant/store.py
@@ -2,24 +2,49 @@ import queue
 import pyodbc
 import pandas as pd
 import sqlalchemy
+import time
 
 class Storage:
-    def __init__(self, Q):
+    def __init__(self, Q, savetype):
         self.q = Q
+        self.started = False 
+        
+        self.savetype = savetype
+
+    def start(self):
+        self.started = True
+
+    def stop(self):
+        self.started = False
+
+    def run(self, params):
+        self.start()
+        while self.started:
+            if self.savetype == 'DB':
+                server , database, table_name = params
+                self.save_to_db(server,database, table_name)
+            elif self.savetype =='csv':
+                path , filename = params
+                self.save_to_csv(path,filename)
+                
+            
 
     def take_off_q(self):
+        while self.q.empty():
+            time.sleep(2)
+            
         data = self.q.get(block = False, timeout = None)
         return data
 
     def save_to_csv(self,path,filename):
         filepath = path + filename
         data = self.take_off_q()
-        data.to_csv(filepath)
+        data.to_csv(filepath, mode = 'a')
          
-    def save_to_db(self,server,database):
+    def save_to_db(self,server,database, table_name):
         engine = sqlalchemy.create_engine('mssql+pyodbc://@' + f'{server}' + '/' + f'{database}' + '?trusted_connection=yes&driver=ODBC Driver 17 for SQL Server')
         data = self.take_off_q()
-        data.to_sql(f'{database}', engine,if_exists='append')
+        data.to_sql(f'{table_name}', engine,if_exists='append')
 
 
     
--- TickerPlant/yahoo_handler.py
+++ TickerPlant/yahoo_handler.py
@@ -7,14 +7,16 @@ import queue
 from store import Storage
 from symbols import Symbol_map
 import configparser
+import threading 
+import time
 
 
 class handler:
     def __init__(self, Q, symbols):
         self.q = Q
         self.symbol = symbols
-        self.start = False 
-        self.stop = True 
+        self.started = False 
+        
 
     def put_on_q(self,data):
         self.q.put(item= data)
@@ -26,34 +28,38 @@ class handler:
             print('queue is empty')
 
     def start(self):
-        self.start = True
+        self.started = True
 
     def stop(self):
-        self.stop = False
+        self.started = False
 
     def run(self):
-        while self.start():
+        self.start()
+        while self.started :
+           
             result = self.get_live_data()
             self.put_on_q(result)
+            time.sleep(60)
 
 
 class yahoo_handler(handler):
 
     def get_hist_data(self):
-
-        start =  dt.datetime.now() - timedelta(days = 730)#dt.datetime(2022,1,1)
-        end = dt.datetime.now()
-        symdf = self.symbol.get_symbols()
-        symlist = symdf['0'].to_list()
-        marketdata = yf.download(symlist, start, end, interval='1m')
-        df = marketdata.stack(level=1).rename_axis(['Date', 'Ticker']).reset_index(level=0)
-        df= df.reset_index()
-        return df
+            start =  dt.datetime.now() - timedelta(days = 730)
+            
+            end = start + timedelta(days=count)
+            symdf = self.symbol.get_symbols()
+            symlist = symdf['0'].to_list()
+            marketdata = yf.download(symlist, start, end, interval='1m')
+            df = marketdata.stack(level=1).rename_axis(['Date', 'Ticker']).reset_index(level=0)
+            df= df.reset_index()
+           
+            return df1
 
 
     def get_live_data(self):
 
-        start =  dt.datetime.now() - timedelta(minutes= 1)#dt.datetime(2022,1,1)
+        start =  dt.datetime.now() - timedelta(minutes= 1)
         end = dt.datetime.now()
         symdf = self.symbol.get_symbols()
         symlist = symdf['0'].to_list()
@@ -84,15 +90,27 @@ def main():
    q = queue.Queue()
    y = Symbol_map(feedType='DB', dbConnection=('LOUIS-PC', 'Crypto_market_prices'), dbtype="MSSQL")
    x = yahoo_handler(Q=q, symbols=y)
-   x.get_data()
-   x.q_check(q)
-   y = Storage(q)
-   y.save_to_csv(path=config['FILEPATH']['Crypto_Data'], filename='test' )
+   z = Storage(q,'csv')
+   hist = x.get_hist_data()
+   x.put_on_q(hist)
+   params = (config['FILEPATH']['Crypto_Data'], '1_min_interval_crypto_data')
+   threading.Thread(target=x.run).start()
+   
+   z.run(params)
+  
+
+
+
+
+
+  
+   
   
    
    
 
 if __name__ == "__main__":
     main()
+
 
 
