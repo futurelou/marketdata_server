f04a1ac1b5dd652e1f181e885ecd5a43bcfe9318
Louie Constable <Lzc5535@psu.edu>
2023-08-21 13:52:01 -0400

    more changes

--- TickerPlant/yahoo_handler.py
+++ TickerPlant/yahoo_handler.py
@@ -2,14 +2,19 @@ import requests
 import pandas as pd
 import yfinance as yf
 import datetime as dt
-from requests_html import HTMLSession
+from datetime import timedelta
 import queue
 from store import Storage
+from symbols import Symbol_map
+import configparser
 
 
 class handler:
-    def __init__(self, Q):
+    def __init__(self, Q, symbols):
         self.q = Q
+        self.symbol = symbols
+        self.start = False 
+        self.stop = True 
 
     def put_on_q(self,data):
         self.q.put(item= data)
@@ -20,38 +25,56 @@ class handler:
         else:
             print('queue is empty')
 
-class yhandler(handler):
+    def start(self):
+        self.start = True
 
-    def get_data(self):
+    def stop(self):
+        self.stop = False
 
-        session = HTMLSession()
-        num_currencies=250
-        resp = session.get(f"https://finance.yahoo.com/crypto?offset=0&count={num_currencies}")
-        tables = pd.read_html(resp.html.raw_html)               
-        df = tables[0].copy()
-        symbols_yf = df.Symbol.tolist()
-        start = dt.datetime(2020,1,1)
+    def run(self):
+        while self.start():
+            result = self.get_live_data()
+            self.put_on_q(result)
+
+
+class yahoo_handler(handler):
+
+    def get_hist_data(self):
+
+        start =  dt.datetime.now() - timedelta(days = 730)#dt.datetime(2022,1,1)
         end = dt.datetime.now()
-        marketdata = yf.download(symbols_yf, start, end)
+        symdf = self.symbol.get_symbols()
+        symlist = symdf['0'].to_list()
+        marketdata = yf.download(symlist, start, end, interval='1m')
         df = marketdata.stack(level=1).rename_axis(['Date', 'Ticker']).reset_index(level=0)
         df= df.reset_index()
-        self.put_on_q(df)
+        return df
+
+
+    def get_live_data(self):
+
+        start =  dt.datetime.now() - timedelta(minutes= = 1)#dt.datetime(2022,1,1)
+        end = dt.datetime.now()
+        symdf = self.symbol.get_symbols()
+        symlist = symdf['0'].to_list()
+        marketdata = yf.download(symlist, start, end, interval='1m')
+        df = marketdata.stack(level=1).rename_axis(['Date', 'Ticker']).reset_index(level=0)
+        df= df.reset_index()
+        return df
+
+    
 
 
 class binance_data(handler):
+
     pass
+
 
 class coin_market_cap(handler):
     pass
 
-class twitter_sentiment:
+class twitter_sentiment(handler):
     pass 
-
-import configparser
-
-
-
-
 
 
 def main():
@@ -59,7 +82,12 @@ def main():
    configFilePath = r'C:\Users\louie\crypto_trade\config.cfg'
    config.read(configFilePath)
    q = queue.Queue()
-   
+   y = Symbol_map(feedType='DB', dbConnection=('LOUIS-PC', 'Crypto_market_prices'), dbtype="MSSQL")
+   x = yahoo_handler(Q=q, symbols=y)
+   x.get_data()
+   x.q_check(q)
+   y = Storage(q)
+   y.save_to_csv(path=config['FILEPATH']['Crypto_Data'], filename='test' )
   
    
    
